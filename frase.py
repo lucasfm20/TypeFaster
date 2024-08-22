47018325a35be9fbd60804ba132912d4899c6de5
Lucas <lucasfm801@gmail.com>
2024-08-21 21:51:44 -0300

    V1

---
+++ frase.py
@@ -0,0 +1,6 @@
+listaFacil=[
+   "Olá, como vai", "Eu gosto de ler", "Vou ao mercado", "Me ajude com isso", "Estou feliz hoje", "Qual é o seu nome", "O que você quer", "Vamos ao cinema", "Hoje é um bom dia", "Onde fica o banco", "Eu quero um café", "Está chovendo agora", "Você viu o filme", "Eu não entendi isso", "Temos que ir agora", "Estou cansado", "Preciso de ajuda", "Vamos caminhar", "O que você acha", "Onde está o livro", "Pode me ajudar", "Estou no trabalho", "O que você fez hoje", "Que dia é hoje", "Eu vou viajar amanhã", "Está frio aqui", "Vamos para a praia", "Onde está o telefone", "Me mostre o caminho"
+]
+
+
+
---
+++ main.py
@@ -0,0 +1,62 @@
+import time
+import random
+from frase import listaFacil
+
+
+
+
+def type():
+    numAle =random.randint(0,29)
+    
+    print(listaFacil[numAle])
+    time.sleep(0.5)
+    print()
+    tempoIni =time.time()
+    frase = input(f"\n")
+
+    tempoFim =time.time()
+    if frase == listaFacil[numAle]:
+        print()
+        print("acertou")
+
+    else:
+        print()
+        print("errou")
+
+    tempoFinal =tempoFim-tempoIni
+
+    print(f"Levou {tempoFinal}")
+    time.sleep(1.5)
+
+def pontua():
+   print()
+   print("Pontuação < 1s = Impossível")
+   print("Pontuação < 2s = Lenda")
+   print("Pontuação < 3s = Ótimo")
+   print("Pontuação < 4s = Bom")
+   print("Pontuação < 5s = Da pra melhorar")
+
+def menu():
+   while True:
+    print("")
+    print("*"*15)
+    print("Menu")
+    print("1-Iniciar")
+    print("2-Pontuação")
+    print("3-Sair")
+    print("*"*15)
+    print("")
+
+    escolha = int(input("Digite uma opcao:"))
+
+    if escolha == 1:
+        type()
+    elif escolha == 2:
+        pontua()
+    elif escolha == 3:
+        break
+    else:
+       print("Informe um número correto")
+
+
+menu()
